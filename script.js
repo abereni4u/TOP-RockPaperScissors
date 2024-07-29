4c77d5750cbfa99a0b97f623dfc472380a23e451
abereni4u <aberenijopuiyo@gmail.com>
2024-07-28 23:31:51 -0400

    Add logic to get human choice

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
+
+const acceptableValues = new Set(["r", "s", "p"]);
+
 function getComputerChoice(){
     let randomValue = Math.random() * 100;
 
     if(randomValue > 0 && randomValue < 33){
-        return "Rock";
+        return "rock";
     }
     else if (randomValue > 33 && randomValue < 66){
-        return "Scissors";
+        return "scissors";
     }
     else{
-        return "Paper";
+        return "paper";
     }
 }
+
+function getHumanChoice(){
+    let humanChoice = prompt("Please enter your move (r for Rock | s for Scissors | p for Paper) :");
+
+    humanChoice.toLowerCase();
+
+    // while input not in valid choices
+    while(!acceptableValues.has(humanChoice)){
+        alert("Invalid input!");
+        humanChoice = prompt("Please enter your move (r for Rock | s for Scissors | p for Paper) :")
+    }
+
+    if(humanChoice === "r"){
+        humanChoice = "rock";
+    }
+    else if (humanChoice === "s"){
+        humanChoice = "scissors";
+    }
+    else if (humanChoice === "p"){
+        humanChoice = "paper";
+    }
+   
+
+    return humanChoice;
+
+}
+
+console.log(getHumanChoice());
